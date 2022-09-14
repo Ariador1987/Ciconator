b102034008ac63e7b7d49c9f032770a52b3f09fb
Božidar Bralić <bozidar.bralic@gmail.com>
2022-09-14 23:56:14 +0200

    about us skoro gotov, rješit btn

--- src/js/main.js
+++ src/js/main.js
@@ -2,12 +2,14 @@ const togglerBtn = document.querySelector(".main-nav--toggler");
 const linksContainer = document.querySelector(".main-nav__links-container");
 const hamburgerLines = document.querySelectorAll(".main-nav--toggler-line");
 
-togglerBtn.addEventListener("click", () => {
+togglerBtn.addEventListener("click", (e) => {
+    e.preventDefault();
     hamburgerLines.forEach((el) => el.classList.toggle("active"));
     linksContainer.classList.toggle("active");
 });
 
-linksContainer.addEventListener("click", () => {
+linksContainer.addEventListener("click", (e) => {
+    e.preventDefault();
     if ([...hamburgerLines].some((el) => el.classList.contains("active"))) {
         hamburgerLines.forEach((el) => el.classList.remove("active"));
         linksContainer.classList.remove("active");
