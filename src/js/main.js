e277a31010eba2f578558a5c63cffd8fb8c4e633
Božidar Bralić <bozidar.bralic@gmail.com>
2022-09-15 23:55:20 +0200

    dodao accordione

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,8 @@
 const togglerBtn = document.querySelector(".main-nav--toggler");
 const linksContainer = document.querySelector(".main-nav__links-container");
 const hamburgerLines = document.querySelectorAll(".main-nav--toggler-line");
+const accordions = document.querySelectorAll(".accordion");
+const accordionContents = document.querySelectorAll(".accordion-content");
 
 togglerBtn.addEventListener("click", (e) => {
     e.preventDefault();
@@ -14,4 +16,36 @@ linksContainer.addEventListener("click", (e) => {
         hamburgerLines.forEach((el) => el.classList.remove("active"));
         linksContainer.classList.remove("active");
     }
+});
+
+accordions.forEach((acc) =>
+    acc.addEventListener("click", (e) => {
+        handleAccordions(acc, e);
+    })
+);
+
+function handleAccordions(accordion, event) {
+    if (!event.target.classList.contains("accordion-heading--icon")) return;
+
+    event.target.textContent.trim() === "+"
+        ? (event.target.textContent = "-")
+        : (event.target.textContent = "+");
+
+    if (
+        accordion.firstElementChild.nextElementSibling.classList.contains(
+            "hidden"
+        )
+    ) {
+        accordion.firstElementChild.nextElementSibling.classList.remove(
+            "hidden"
+        );
+        accordion.firstElementChild.nextElementSibling.classList.add("show");
+    } else {
+        accordion.firstElementChild.nextElementSibling.classList.remove("show");
+        accordion.firstElementChild.nextElementSibling.classList.add("hidden");
+    }
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+    [...accordionContents].forEach((el) => el.classList.add("hidden"));
 });
