1b8dfc89b88bb93a9ab773bf15074cbcb7dd13d9
Božidar Bralić <bozidar.bralic@gmail.com>
2022-09-28 20:30:47 +0200

    working on gallery

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,6 @@
 import "./testimonials.js";
 import "./accordion.js";
+import "./gallery.js";
 
 const togglerBtn = document.querySelector(".main-nav--toggler");
 const linksContainer = document.querySelector(".main-nav__links-container");
